ad6d608873c5c45f542054ceac3be77000d81fd2
tatsuki199 <tanukityomari@gmail.com>
2024-09-16 19:07:45 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,5 @@ class UsersController < ApplicationController
 
   def user_params
      params.require(:user).permit(:nickname, :email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth)
-  # end
+  end
 end
